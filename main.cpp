eac5f1456de9603d859da72e263160c0a83bb0d2
YannosK <giannos2405@gmail.com>
2023-06-30 13:13:06 +0300

    My main branch will use serial for ssid & pswd

--- main.cpp
+++ main.cpp
@@ -1,35 +1,68 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#define ledPIN 5
 
-const char* ssid = "MyrDyn"; //my ssid
-const char* password = "M90b53M98pj62@"; //password
+void ConnectToWiFi(const char* WIFI_NETWORK, const char* WIFI_PASSWORD);
+
+int status = WL_IDLE_STATUS;
 
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPIN,OUTPUT);
-  WiFi.begin(ssid, password);
+
+  char WiFi_ssid[32];
+  char WiFi_pswd[32];
+
+  Serial.print("\nAdd WiFi SSID: ");
+  while(!Serial.available()){
+    delay(1);
+  }
+  Serial.readBytes(WiFi_ssid, sizeof(WiFi_ssid));
+  WiFi_ssid[sizeof(WiFi_ssid) - 1] = '\0';
+  String ssidString = WiFi_ssid;
+  ssidString.trim();  // Remove leading/trailing whitespaces
+  ssidString.toCharArray(WiFi_ssid, sizeof(WiFi_ssid));
+  Serial.print("\nWiFi_ssid: ");
+  Serial.println(WiFi_ssid);
+
+  Serial.print("\nAdd WiFi password: ");
+  while(!Serial.available()){
+    delay(1);
+  }
+  Serial.readBytes(WiFi_pswd, sizeof(WiFi_pswd));
+  WiFi_pswd[sizeof(WiFi_pswd) - 1] = '\0';
+  String pswdString = WiFi_pswd;
+  pswdString.trim();  // Remove leading/trailing whitespaces
+  pswdString.toCharArray(WiFi_pswd, sizeof(WiFi_pswd));
+  Serial.print("\nWiFi_pswd: ");
+  Serial.println(WiFi_pswd);
+
+  ConnectToWiFi(WiFi_ssid, WiFi_pswd);
+}
+
+
+void loop() {
+  Serial.print("\nIP address: ");
+  Serial.println(WiFi.localIP());
+  delay(1000);
+}
+
+
+void ConnectToWiFi(const char* WIFI_NETWORK, const char* WIFI_PASSWORD) {
   Serial.print("Connecting to WiFi");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
   
   while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(500);
   }
-  Serial.print("\nConected to WiFi network with local IP address:");
-  Serial.println(WiFi.localIP());
-}
 
-void loop() {  
-  if (WiFi.status() == WL_CONNECTED){
-    Serial.println("LED ON");
-    digitalWrite(ledPIN,HIGH);
-    delay(1000);
-    Serial.println("LED OFF");
-    digitalWrite(ledPIN,LOW);
-    delay(1000);
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.println("Connection failed!");
   }
   else{
-    Serial.println("Connection lost");
+    Serial.print("\nConected to WiFi network with local IP address:");
+    Serial.println(WiFi.localIP()); 
   }
 }
