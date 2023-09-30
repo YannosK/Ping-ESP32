c69567697e50ae55bbdff70d26782b246da2e758
YannosK <giannos2405@gmail.com>
2023-09-30 22:12:38 +0300

    WiFi server client for rpi

--- main.cpp
+++ main.cpp
@@ -20,9 +20,16 @@ ARTINA
 
 void ConnectToWiFi(const char* WIFI_NETWORK, const char* WIFI_PASSWORD);
 void WifiCredentialsViaSerial(char WiFi_network[], char WiFi_password[]);
+void ConnectToServer(char server[]);
 
 int status = WL_IDLE_STATUS;
 
+char server[] = "www.google.com";
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
@@ -33,13 +40,29 @@ void setup() {
   WifiCredentialsViaSerial(WiFi_ssid, WiFi_pswd);
 
   ConnectToWiFi(WiFi_ssid, WiFi_pswd);
+
+  ConnectToServer(server);
 }
 
 
 void loop() {
+  /*
   Serial.print("\nIP address: ");
   Serial.println(WiFi.localIP());
   delay(1000);
+  */
+
+  while(client.connected()){
+    // if there are incoming bytes available from the server, read them and print them:
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+    }    
+  }
+
+  Serial.println();
+  Serial.println("disconnecting from server.");
+  client.stop();
 }
 
 
@@ -62,6 +85,7 @@ void ConnectToWiFi(const char* WIFI_NETWORK, const char* WIFI_PASSWORD) {
     Serial.println(WiFi.localIP()); 
   }
 }
+
 
 void WifiCredentialsViaSerial(char WiFi_network[], char WiFi_password[]) {
   Serial.print("\nAdd WiFi SSID: ");
@@ -87,4 +111,19 @@ void WifiCredentialsViaSerial(char WiFi_network[], char WiFi_password[]) {
   pswdString.toCharArray(WiFi_password, 32);
   Serial.print("\nWiFi_password: ");
   Serial.println(WiFi_password);
+}
+
+
+void ConnectToServer(char server[])
+{
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
 }
